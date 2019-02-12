1b324a7ba7139ba29c132109a8df459e39267368
Akash Kumar <akash.kumar@jobcloud.ch>
2019-02-12 16:35:26 +0100

    try php approach (clean)

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Overview with links to the single emails would go here.');
-});
-
-module.exports = router;
