652c6e4ce27cb4d7ed511e5de5d5358b0d314e17
Akash Kumar <akash.kumar@jobcloud.ch>
2019-02-12 16:29:15 +0100

    some trying around

--- routes/job-recommendation.js
+++ routes/job-recommendation.js
@@ -14,9 +14,13 @@ Twig.extendFunction("asset", function (asset, location) {
 });
 
 Twig.extendFilter("addTrackingParameters", function (templateName, urlType, userId, messageId) {
-    return "";
+    return "?foo=bar";
 });
 
+Twig.extendFilter("trans", function (a, b, c) {
+    console.log(a, b, c);
+    return a;
+});
 
 var browserMode = true;
 
@@ -26,10 +30,15 @@ router.get('/', function (req, res, next) {
         if (browserMode) {
             // render mjml to html and render twig to show the finished email in the browser
             var template = twig({
-                data: data
+                data: data,
+                allowInlineIncludes: true
             });
 
             var mjmlButRenderedTwig = template.render({
+                headline: "Job-Empfehlungen",
+                lead: "Schau mal, wir haben folgende Jobs gefunden, die zu dir passen:",
+                logoImg: "https://jobs.ch/asset/img/mail/jobs-ch.gif",
+                logoLink: "https://jobs.ch/en/?utm_medium=organic&amp;utm_campaign=ch_recommended-jobs_b2c_jobs.ch&amp;utm_content=job-link-logo&amp;source=dsp_mail&amp;uid=29a683b0-f94e-49dd-8d6b-4612775566f3&amp;mid=bb4b982b-ad61-4c01-bf09-3333e076beac",
                 jobs: [{
                         "id": "f17817a2-2307-4eaf-a59c-f9b6d971e89c",
                         "title": "Test Job 1",
